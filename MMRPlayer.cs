d953f461ade40ac1bf599d9e078061c7a7970579
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-10-24 01:32:57 -0700

    Partially Port BoC Changes

---
+++ MMRPlayer.cs
@@ -0,0 +1,38 @@
+﻿using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.ModLoader;
+
+namespace MasterModeReloaded
+{
+    public class MMRPlayer : ModPlayer
+    {
+        #region Player Fields
+        //Buffs/Debuffs
+        public bool trueConfusion;
+        //Accessories
+
+        //Misc.
+        #endregion
+
+        public override void ResetEffects()
+        {
+            trueConfusion = false;
+        }
+
+        #region Buff Related
+        public override void PostUpdateBuffs()
+        {
+            /*if (trueConfusion)
+            {
+                Filters.Scene.Activate("VerticalMirror");
+            }
+            else
+            {
+                Filters.Scene.Deactivate("VerticalMirror");
+            }*/
+        }
+        #endregion
+    }
+}
